373814dfc2e5a14acefc57d29c329c43be70cfea
AbyChan <crystal.tyan@gmail.com>
2016-05-08 12:20:23 +0800

    inital commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# nobbb-parse-md
--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
 'use strict';
 
-let org = require('markdown');
+let markdown = require('markdown').markdown;
 
-export default class NobbbParseOrg {
+let R = require('fw-ramda');
+
+export default class NobbbParseMarkdown {
     constructor() {
         this.name = 'markdown';
     }
 
     check(file) {
-        return /\.markdown$/.test(file);
+        return /\.md$/.test(file);
     }
     
     parse(mdCode) {
-
         let lines = mdCode.split('\n');
-        for (let i = 0; ; i++) {
-            
+        let infos = {};
+        let i = 0;
+        for (; ; i++) {
+            if( /^<!--.+-->$/.test(lines[i]) ){
+
+                let info = lines[i].substring(4, lines[i].length - 3);
+
+                let un = info.split(':'),
+                    key = un[0].toLowerCase(),
+                    value = R.drop(1, un).join(':');
+
+                infos[key] = value;
+                console.log(infos);
+            } else {
+                break;
+            }
         }
         
         return {
-            
+            title: infos.title,
+            content: markdown.toHTML(R.drop(i, lines).join('\n')),
+            type: this.name
         };
     }
 }
